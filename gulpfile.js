cb9298277a621ca6767605e3701de8edb3c98e56
VovanR <mail@vovanr.com>
2015-03-29 12:22:47 +0300

    Fix throwing gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 
 gulp.task('test', function () {
-    gulp.src('./test/**/*.js', { read: false })
+    return gulp.src('./test/**/*.js', { read: false })
         .pipe(mocha({
             reporter: 'nyan',
         }));
