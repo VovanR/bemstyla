99a8a80bbcf3e241d42164e1e190a22041eb8108
VovanR <mail@vovanr.com>
2015-04-04 21:07:10 +0300

    Creator: +`getFilePaths`

--- test/creator-test.js
+++ test/creator-test.js
@@ -5,6 +5,7 @@ var yaml = require('js-yaml');
 var fs = require('fs');
 
 var testData = yaml.safeLoad(fs.readFileSync('./test/creator-test-cases.yml', 'utf8'));
+var getFilePathsTestData = yaml.safeLoad(fs.readFileSync('./test/creator-getFilePaths-test-cases.yml', 'utf8'));
 
 describe('creator', function () {
     it('should be `Object`', function () {
@@ -17,6 +18,17 @@ describe('creator', function () {
                 assert.equal(
                     creator.getPath(data.data),
                     data.path
+                );
+            });
+        });
+    });
+
+    describe('#getFilePaths', function () {
+        it('should parse input to file names and paths', function () {
+            _.forEach(getFilePathsTestData, function (data) {
+                assert.deepEqual(
+                    creator.getFilePaths(data.input),
+                    data.output
                 );
             });
         });
