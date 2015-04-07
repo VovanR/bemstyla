de05d721089858aaffb4a73adcf9aa70a452c98c
VovanR <mail@vovanr.com>
2015-04-07 21:47:55 +0300

    Clear Creator

--- test/creator-test.js
+++ test/creator-test.js
@@ -3,34 +3,34 @@ var creator = require('../lib/creator');
 var _ = require('lodash');
 var yaml = require('js-yaml');
 var fs = require('fs');
+var path = require('path');
+var exists = fs.exists || path.exists;
+var mkdirp = require('mkdirp');
 
-var testData = yaml.safeLoad(fs.readFileSync('./test/creator-test-cases.yml', 'utf8'));
-var getFilePathsTestData = yaml.safeLoad(fs.readFileSync('./test/creator-getFilePaths-test-cases.yml', 'utf8'));
+var testData = yaml.safeLoad(fs.readFileSync('./test/format-file-test-cases.yml', 'utf8'));
 
 describe('creator', function () {
     it('should be `Object`', function () {
         assert.isObject(creator);
     });
 
-    describe('#getPath', function () {
-        it('should format path from input string', function () {
-            _.forEach(testData, function (data) {
-                assert.equal(
-                    creator.getPath(data.data),
-                    data.path
-                );
-            });
-        });
-    });
-
-    describe('#getFilePaths', function () {
-        it('should parse input to file names and paths', function () {
-            _.forEach(getFilePathsTestData, function (data) {
-                assert.deepEqual(
-                    creator.getFilePaths(data.input),
-                    data.output
-                );
-            });
-        });
-    });
+    // describe('#touchFiles', function () {
+    //     it('should create `styl` files', function (done) {
+            // mkdirp.sync('/tmp/' + creator.getPath('block__elem_mod-name_mod-val'));
+            // exists('/tmp/block/__elem/_mod-name', function (ex) {
+            //     assert.ok(ex, 'Dir created');
+            // });
+            // var paths = creator.getFilePaths('block__elem_mod-name_mod-val');
+            // var stream = fs.createWriteStream('/tmp/' + paths.block.file + '.styl');
+            // stream.once('open', function(fd) {
+            //     stream.write('.' + paths.block.name + '\n    {}\n');
+            //     stream.end();
+            //     done();
+            // });
+            // fs.accessSync('temp', fs.W_OK, function (err) {
+            //     assert.isFalse(err);
+            //     done();
+            // });
+    //     });
+    // });
 });
