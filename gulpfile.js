ad9db17f97a8317abcef095b30d349e3aecb7d52
VovanR <mail@vovanr.com>
2015-04-15 12:07:42 +0300

    Restructure: return gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,7 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 
 gulp.task('test', function () {
-    return gulp.src([
-            './test/parser-test.js',
-            './test/format-file-test.js',
-            './test/format-file-name-test.js',
-            './test/format-file-dir-test.js',
-            './test/creator-test.js',
-        ], { read: false })
+    return gulp.src('./test/**/*.js', { read: false })
         .pipe(mocha({
             reporter: 'nyan',
         }));
