861662c2efc5560a4cf7a641e504cd9411e18431
Vladimir Rodkin <mail@vovanr.com>
2015-04-30 11:57:24 +0300

    Templates parser (Jade, HTML) #4
    * jadeParser: rename method `parse` -> `parseString`
    * jadeParser: +`parseFile` method and tests

--- test/parser-jade-test.js
+++ test/parser-jade-test.js
@@ -1,28 +1,52 @@
 var assert = require('chai').assert;
+var mockfs = require('mock-fs');
 var parserJade = require('../lib/parser-jade');
 var _ = require('lodash');
 var yaml = require('js-yaml');
 var fs = require('fs');
+var path = require('path');
 
 var testData = yaml.safeLoad(fs.readFileSync('./test/parser-jade-test-cases.yml', 'utf8'));
+
+var TEMP_DIR = '/tmp/bemstyla';
 
 describe('parserJade', function () {
     it('should be `Object`', function () {
         assert.isObject(parserJade);
     });
 
-    describe('#parse', function () {
+    describe('#parseString', function () {
         it('should return an `Array`', function () {
-            assert.isArray(parserJade.parse());
+            assert.isArray(parserJade.parseString());
         });
 
         it('should parse class names from jade text', function () {
             _.forEach(testData, function (data) {
                 assert.deepEqual(
-                    parserJade.parse(data.input),
+                    parserJade.parseString(data.input),
                     data.output
                 );
             });
+        });
+    });
+
+    describe('#parseFile', function () {
+        it('should return an empty array if file does not exists', function () {
+            assert.isArray(parserJade.parseFile());
+        });
+
+        it('should parse Jade file', function () {
+            var mockData = {};
+
+            var filePath = path.join(TEMP_DIR, 'foo.jade');
+            var fileText = '.foo.bar';
+            mockData[filePath] = fileText;
+            mockfs(mockData);
+
+            assert.deepEqual(fs.readFileSync(filePath).toString(), fileText);
+            assert.deepEqual(parserJade.parseFile(filePath), ['foo', 'bar']);
+
+            mockfs.restore();
         });
     });
 });
