d3c9a4bd6067e0759f6129963481ed0cbe682ca2
Vladimir Rodkin <mail@vovanr.com>
2015-12-17 22:17:42 +0300

    Verify that the directory exists #21
    * Dir exists
    * Write permissions

--- bin/bemstyla.js
+++ bin/bemstyla.js
@@ -4,6 +4,8 @@
 var program = require('commander');
 var _ = require('lodash');
 var pkg = require('../package.json');
+var Promise = require('bluebird');
+var fs = require('fs');
 
 program
 	.version(pkg.version)
@@ -26,12 +28,49 @@ if (program.args.length < 1) {
 	program.help();
 }
 
-var index = require('../lib/index');
-
-_.forEach(program.args, function (arg) {
-	index({
-		source: arg,
-		fileType: program.type,
-		baseDir: program.dir
+function checkExists(path) {
+	return new Promise(function (resolve, reject) {
+		fs.stat(path, function (err, stats) {
+			if (!err || (stats && stats.isDirectory())) {
+				return resolve(path);
+			}
+			console.log(err.message);
+			return reject();
+		});
 	});
-});
+}
+
+function checkAccess(path) {
+	return new Promise(function (resolve, reject) {
+		fs.access(path, fs.R_OK | fs.W_OK, function (err) {
+			if (!err) {
+				return resolve();
+			}
+			console.error(err.message);
+			return reject();
+		});
+	});
+}
+
+function start() {
+	var index = require('../lib/index');
+
+	_.forEach(program.args, function (arg) {
+		index({
+			source: arg,
+			fileType: program.type,
+			baseDir: program.dir
+		});
+	});
+}
+
+if (program.dir) {
+	checkExists(program.dir)
+		.then(checkAccess)
+		.then(function () {
+			start();
+		})
+		.catch(function () {});
+} else {
+	start();
+}
